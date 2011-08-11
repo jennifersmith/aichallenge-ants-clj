762bd426be6ced848e7bd1b26f81879b60f4c8f8
Jennifer Smith <jennifer@home>
2011-08-10 22:22:51 -0500

    Now reading in turn data

--- src/jbot/core.clj
+++ src/jbot/core.clj
@@ -17,6 +17,22 @@
            (apply (partial assoc result) (string/split #"\s" line)))
            {}           
             lines)))
+(def code->event-type 
+  {"a" :live-ant
+   "d" :dead-ant
+   "f" :food
+   "w" :water})
+
+(defn read-turn-input 
+  ([event-type x y player] (assoc (read-turn-input event-type x y) :player player))
+  ([event-type x y] {:type (code->event-type event-type) :pos {:x (Integer/parseInt x) :y (Integer/parseInt y)}}))
+
+(defn read-turn-data [reader]
+  (let [lines (read-upto "go" reader)]
+    (map (fn [line] (apply read-turn-input (string/split #"\s" line))) lines)))
 
 (defn -main [& args]
-  (let [parameters (read-parameters *in*)] "todo : play game"))
+  (let [parameters (read-parameters *in*)] 
+    (println "go")
+    
+    (read-upto "end" *in*)))
--- test/jbot/core_test.clj
+++ test/jbot/core_test.clj
@@ -12,3 +12,12 @@
 
 (fact
   (read-parameters (create-reader "Foo bar" "Baz 123", "ready"))=> {"Foo" "bar", "Baz" "123"})
+
+(fact
+  (read-turn-data (create-reader "f 20 20" "w 10 10" "a 10 10 1" "d 6 6 4" "go")) =>
+  (just
+   {:type :food :pos {:x 20 :y 20}}
+   {:type :water :pos {:x 10 :y 10}}
+   {:player "1" :type :live-ant :pos {:x 10 :y 10}}
+   {:player "4" :type :dead-ant :pos {:x 6 :y 6}}
+    :in-any-order))
