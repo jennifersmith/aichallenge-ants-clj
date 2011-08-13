2d344775503e8e39134cf5351482263cfac93352
Jennifer Smith <jennifer@home>
2011-08-13 09:58:34 -0500

    moved some bits around

--- src/jbot/core.clj
+++ src/jbot/core.clj
@@ -2,16 +2,34 @@
   (:gen-class)
   (:require [clojure.pprint :as pp] [clojure.contrib.logging :as logs] [ clojure.contrib.string :as string])
   (:import (java.io BufferedReader)))
-(defn tokenize [line]
-  (string/split #"\s" line))
-
-(defn dump [& data]
-  (spit "dump.log" (apply str (cons "\n" (interpose " " data))) :append true))
-
+;; too lazy to split into files!
+(defn dump [& data](spit "dump.log" (apply str (cons "\n" (interpose " " data))) :append true))
 (defmacro dbg[x] `(let [x# ~x] (dump '~x "=" x#) x#))
 
+;; string utils
+(defn tokenize [line] (string/split #"\s" line))
+
+;; Protocol parsing stuff (not as in clojure protocols...) - this stuff takes tokenized strings and gets the goodies out
+;; Should I maybe assert "turn" as in "turn 2" and "end" as in the end of the game?
+
+(def code->event-type 
+  {"a" :live-ant
+   "d" :dead-ant
+   "f" :food
+   "w" :water})
+
+(defn read-turn-header
+  ([turn-message turn-number] (Integer/parseInt turn-number))
+  ([end-message] nil))
+
+
+(defn read-turn-input 
+  ([event-type x y player] (assoc (read-turn-input event-type x y) :player player))
+  ([event-type x y] {:type (code->event-type event-type) :pos {:x (Integer/parseInt x) :y (Integer/parseInt y)}}))
+
+;; in stream reading stuff
 (defn read-upto [stop-token]
-    (loop [result []]
+  (loop [result []]
       (let [line (read-line)] 
         (dump line)
         (if (or (nil? line) (= stop-token line))
@@ -25,32 +43,17 @@
            {}           
             lines)))
 
-(def code->event-type 
-  {"a" :live-ant
-   "d" :dead-ant
-   "f" :food
-   "w" :water})
-
-(defn read-turn-input 
-  ([event-type x y player] (assoc (read-turn-input event-type x y) :player player))
-  ([event-type x y] {:type (code->event-type event-type) :pos {:x (Integer/parseInt x) :y (Integer/parseInt y)}}))
-
 (defn read-turn-data []
   (let [lines (read-upto "go")]
     (map (fn [line] (apply read-turn-input (tokenize line))) lines)))
-
-;; Should I maybe assert "turn" as in "turn 2" and "end" as in the end of the game?
-(defn read-turn-header
-  ([turn-message turn-number]
-  (dump turn-message turn-number)
-   (Integer/parseInt turn-number))
-  ([end-message] nil))
 
 (defn read-turn []
   (if-let [turn-header (apply read-turn-header (tokenize (read-line)))]
     {:turn-number turn-header
      :turn-data (dbg (read-turn-data))}))
 
+;; Main!
+ 
 (defn -main [& args]
   (let [parameters (read-parameters)] 
     (do (println "go") (.flush System/out)) 
