50de3fc85afa1d48b7697624bf8c3885e7b36ae0
Jennifer Smith <jennifer@home>
2011-08-14 13:20:36 -0500

    Initialised world and incremented in a sensible way...:

--- src/world.clj
+++ src/world.clj
@@ -1,9 +1,27 @@
 (ns world)
+(defn dump-1 [& data] (println (apply str data)))
 
-(defn get-knowledge [knowledge-type new-information]
-  (map :pos (filter #(= knowledge-type (:type %)) new-information)))
-(defn increase-knowledge-of [knowledge-type new-information current-knowledge]
-  (concat current-knowledge (get-knowledge knowledge-type new-information)))
+(defmacro dbg-1[x] `(let [x# ~x] (dump-1 '~x "=" x#) x#))
+(defn pivot [filter-key required-values data]
+  (zipmap required-values (map
+    (fn [current-key]
+      (filter
+        (fn [current] (= current-key (get current filter-key)))
+        data))
+    required-values)))
 
-(defn forget-about [knowledge-type new-information current-knowledge]
-  (remove (set (get-knowledge knowledge-type new-information)) current-knowledge))
+(defn init-world [] {:player-name "0" :food [] :water [] :my-ants[]})
+
+(defn increment-world [current-state new-information]
+  (let [
+        new-information-by-type (pivot :type [:food :water :ant] new-information)
+        new-water (map :pos (:water new-information-by-type))
+        new-food (map :pos (:food new-information-by-type))
+        player-name (:player-name current-state)
+        ants (map :pos (filter #(= player-name (:player  %)) (:ant new-information-by-type)))
+        ]
+    (-> current-state
+      (assoc :water (concat (:water current-state) new-water))
+      (assoc :food new-food)
+      (assoc :my-ants ants))))
+
--- test/jbot/world_test.clj
+++ test/jbot/world_test.clj
@@ -5,15 +5,34 @@
         clojure.test
         midje.sweet))
 
-(fact "create knowledge for one param"
-      (increase-knowledge-of :water [{:type :ant :pos [1 2]} {:type :water :pos [4 5]} {:type :water :pos [14 5]}] [])
-      => [[4 5] [14 5]])
+(fact (init-world) => {:player-name "0" :water [] :food [] :my-ants []})
 
-(fact "build on top of existing knowledge"
-      (increase-knowledge-of :food [{:type :food :pos [1 2]} {:type :water :pos [2 1]}] [[1 1] [5 5]])
-      => [[1 1] [5 5] [1 2]])
+(fact "pivoting a set of hashes on a key"
+      (pivot :name [:foo :bar] [{:name :boris :age 99} {:name :foo :age 20} {:name :foo :age 50} {:name :bar :age 15} {:name :bar :age 19}])
+      => {
+          :foo [{:name :foo :age 20} {:name :foo :age 50}]
+          :bar [{:name :bar :age 15} {:name :bar :age 19}]
+          })
 
-(fact "remove existing knowledge"
-      (forget-about :foo [{:type :foo :pos [5 6]} {:type :foo :pos [7 7]}] [[5 6] [7 7] [19 19]])
-      => [[19 19]])
-                                                                      
+(fact "should be able to use inbound turn data to figure out the world"
+      (increment-world {
+                        :player-name "bob"
+                        :water [[1 2] [3 4]]
+                        :food [[1 2] [5 6]]
+                        :my-ants [[5 1] [2 1]]
+                        }
+                       [
+                        {:type :water :pos [15 50]}
+                        {:type :food :pos [1 2]}
+                        {:type :food :pos [10 10]}
+                        {:type :ant :pos [5 2] :player "bob"}
+                        {:type :ant :pos [2 1] :player "colin"}
+                        ])
+      =>
+      {
+       :player-name "bob"
+       :water [[1 2] [3 4] [15 50]]
+       :food [[1 2] [10 10]]
+       :my-ants [[5 2]]
+       })
+
