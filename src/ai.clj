5e6c2da11494143530248991fa9ab278b51e4323
Jennifer Smith <jennifer@home>
2011-08-16 08:26:57 -0500

    Half assed version of sample bot based on java sample

--- src/ai.clj
+++ src/ai.clj
@@ -1,14 +1,23 @@
-(ns ai)
-
-(defn get-next-passable-direction [surroundings]
-  "N"
-  )  
- 
+(ns ai
+  (:require world))
+;; The whole point of this exercise
+(defn get-next-passable-direction
+  [[
+   [_ N _]
+   [W _ E]
+   [_ S _]
+   ]]
+  (let
+    [
+      directions {"N" N "E" E "S" S "W" W}]
+   (first (map key 
+     (filter (fn [[k v]] (nil? v)) directions))))) ;; hackety hack
 (defn sample-bot[world]
+  (filter
+    (fn [{:keys [direction]}] (not (nil? direction)))
   (map
     (fn [ant-pos]
-      {:pos ant-pos :direction (get-next-passable-direction (get-surroundings world ant-pos))})
-    (:my-ants world)))
- 
+      {:pos ant-pos :direction (get-next-passable-direction (world/get-surroundings world ant-pos))})
+    (:my-ants world))))
 ;; let's hear it for plugability
 (def next-move sample-bot)
