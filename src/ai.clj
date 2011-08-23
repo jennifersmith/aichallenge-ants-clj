52870d8e5bfb004c8dd61d9908e68e4ce04a54ec
Jennifer Smith <jennifer@home>
2011-08-22 22:45:56 -0500

    Finished I think

--- src/ai.clj
+++ src/ai.clj
@@ -2,12 +2,12 @@
   (:use debug)
   (:require game-state))
 
-(defn ant-next-move [game-state rand-generator pos]
-  (let [directions  (game-state/get-available-directions game-state pos)]
-  {:pos pos :direction (nth (vec directions) (rand-generator (count directions)) nil)}))
+(defn ant-next-move [environment random-generator pos]
+  (let [directions  (game-state/get-available-directions environment pos)]
+  {:pos pos :direction (nth (vec directions) (random-generator (count directions)) nil)}))
 
-(defn sample-bot-move [{:keys [my-ants] :as game-state}] 
-  (filter (comp :direction identity) (map (partial ant-next-move game-state (:rand-generator game-state)) my-ants)))
+(defn sample-bot-move [{:keys [my-ants environment random-generator]}] 
+  (filter (comp :direction identity) (map (partial ant-next-move environment random-generator) my-ants)))
 
 ;; let's hear it for plugability
 (def next-move sample-bot-move)
--- test/jbot/ai_test.clj
+++ test/jbot/ai_test.clj
@@ -7,24 +7,27 @@
 
 (def row-of-water [:water :water :water])
 (def row-of-nothing [nil nil nil])
+(defn random-generator [max] (rand max))
+
 
 (fact
   "return a noop if poor ant is stuck"
-  (ant-next-move :game-state (fn [max] 1) [10 10]) => {:pos [10 10] :direction nil}
+  (ant-next-move :environment random-generator [10 10]) => {:pos [10 10] :direction nil}
   (provided
-    (get-available-directions :game-state [10 10])=>()))
+    (get-available-directions :environment [10 10])=>()))
 
 (fact
   "Returns a random available direction using given random generator"
-  (ant-next-move :game-state (fn [max] 1) [10 10]) => {:pos [10 10] :direction :S  }
+  (ant-next-move :environment random-generator [10 10]) => {:pos [10 10] :direction :S  }
   (provided
-    (get-available-directions :game-state [10 10])=>[:E :S]))
+    (rand 2) => 1
+    (get-available-directions :environment [10 10])=>[:E :S]))
 
 (fact "Returns all the valid moves for my ants"
-  (sample-bot-move {:my-ants [:a :b :c] :rand-generator :rand})
+  (sample-bot-move {:my-ants [:a :b :c] :environment :env  :random-generator :rand})
       => [{:direction :N} {:direction :S}]
   (provided
-    (ant-next-move {:my-ants [:a :b :c] :rand-generator :rand } :rand :c) => {:direction nil}
-    (ant-next-move {:my-ants [:a :b :c] :rand-generator :rand} :rand :a) => {:direction :N}
-    (ant-next-move {:my-ants [:a :b :c] :rand-generator :rand } :rand :b) => {:direction :S}))
+    (ant-next-move :env :rand :c)  => {:direction nil}
+    (ant-next-move :env :rand :a)  => {:direction :N}
+    (ant-next-move :env :rand :b) => {:direction :S}))
 
